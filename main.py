be6e7e63c136ba3429f83bfd59637d6fcff0b7c3
spacek531 <spacek531@comcast.net>
2020-11-06 06:36:51 -0800

    initial release

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 
 import main_window
 
 app = QApplication([])
 window = main_window.MainWindow()
 window.show()
+app.setWindowIcon(QIcon(":/resources/tracker.ico"))
+window.setWindowIcon(QIcon(":/resources/tracker.ico"))
 app.exec_()
